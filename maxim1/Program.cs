a409eaa51d229135d43a6f346fcebb04ffd2dda7
79375 <79375@LAPTOP-JJCBPK37>
2024-06-04 20:16:24 +0300

    Добавьте файлы проекта.

---
+++ maxim1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxim1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку");
+            string stroka = Console.ReadLine();
+            Zapis(stroka);
+
+
+        }
+        static void Zapis(string stroka)
+        {
+            if (stroka.Length % 2 == 0)
+            {
+                int index = stroka.Length / 2;
+                string newst = Reverse(stroka.Substring(0, index));
+                string newst2 = Reverse(stroka.Substring(index));
+                Console.WriteLine(newst + newst2);
+                Console.Read();
+            }
+            else
+            {
+                string revst = Reverse(stroka);
+                Console.WriteLine(revst + stroka);
+                Console.Read();
+            }
+        }
+        static string Reverse(string podstroka)
+        {
+            char[] charArray = podstroka.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
---
+++ maxim1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("maxim1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("maxim1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a632f29a-3143-4ee3-ac00-4ad420ba0e0e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
